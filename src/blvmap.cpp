1b94e1d56f65b4b6c24ff52018c24f4e09778627
angeld <roman.andriyanov@corp.mail.ru>
2018-04-20 15:54:45 +0300

    blmMap.h

--- src/blvmap.cpp
+++ src/blvmap.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-#include "mmMap.h"
+#include "blvMap.h"
 #include "zlib.h"
 #include "log.h"
 
@@ -32,74 +32,74 @@ namespace angel{
 
 		{//Vertexs
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Vertex");
-			num_vertex = *(int*)(data+off); off+=4;
-			vertex_data = (short*)(data+off);
-			CHECK_OFF( num_vertex*6 );
+			map_data.num_vertex = *(int*)(data+off); off+=4;
+			map_data.vertex_data = (short*)(data+off);
+			CHECK_OFF( map_data.num_vertex*6 );
 		}
 		{//Faces
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Faces");
-			num_faces = *(int*)(data+off);  off+=4;
-			faces_array = data+off;
-			CHECK_OFF( num_faces * facesize );
-			facedatasize = *(int*)(data+0x68);
-			facedata = (short*)( data+off );
-			CHECK_OFF( facedatasize );
+			map_data.num_faces = *(int*)(data+off);  off+=4;
+			map_data.faces_array = data+off;
+			CHECK_OFF( map_data.num_faces * map_sizes.facesize );
+			map_data.facedatasize = *(int*)(data+0x68);
+			map_data.facedata = (short*)( data+off );
+			CHECK_OFF( map_data.facedatasize );
 
-			facetextures = data + off;
-			CHECK_OFF( num_faces *0x0a );
+			map_data.facetextures = data + off;
+			CHECK_OFF( map_data.num_faces *0x0a );
 
-			numfaceparms = *(int*)(data+off);  off+=4;
+			map_data.numfaceparms = *(int*)(data+off);  off+=4;
 			//faceparams1 = data +off;
-			faceparams1  = (faceparams_blv_t*)(data+off);
-			CHECK_OFF(numfaceparms *0x24);
-			faceparams2 = data +off;
-			CHECK_OFF(numfaceparms *0x0a);
+			map_data.faceparams1  = (blv_faceparams_t*)(data+off);
+			CHECK_OFF(map_data.numfaceparms *0x24);
+			map_data.faceparams2 = data +off;
+			CHECK_OFF(map_data.numfaceparms *0x0a);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Sectors");
-			num_sectors = *(int*)(data+off);off+=4;
-			sectorsdata = data +off;
-			CHECK_OFF(num_sectors*sectorsize);
+			map_data.num_sectors = *(int*)(data+off);off+=4;
+			map_data.sectorsdata = data +off;
+			CHECK_OFF(map_data.num_sectors*map_sizes.sectorsize);
 
-			Rdatasize = *(int*)(data+0x6c);
-			sectorsRdata = data + off;
-			CHECK_OFF(Rdatasize);
-			RLdatasize = *(int*)(data+0x70);
-			sectorsRLdata = data + off;
-			CHECK_OFF(RLdatasize);
+			map_data.Rdatasize = *(int*)(data+0x6c);
+			map_data.sectorsRdata = data + off;
+			CHECK_OFF(map_data.Rdatasize);
+			map_data.RLdatasize = *(int*)(data+0x70);
+			map_data.sectorsRLdata = data + off;
+			CHECK_OFF(map_data.RLdatasize);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Sprites");
-			num_sprites_hz = *(int*)(data+off); off+=4;
-			num_sprites = *(int*)(data+off);off+=4;
-			spritesdata = data + off;
-			CHECK_OFF(num_sprites*spritesize);
-			spritesnamesdata = (char*)(data + off);
-			CHECK_OFF(0x20 * num_sprites);
+			map_data.num_sprites_hz = *(int*)(data+off); off+=4;
+			map_data.num_sprites = *(int*)(data+off);off+=4;
+			map_data.spritesdata = data + off;
+			CHECK_OFF(map_data.num_sprites*map_sizes.spritesize);
+			map_data.spritesnamesdata = (char*)(data + off);
+			CHECK_OFF(0x20 * map_data.num_sprites);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Lights");
-			num_lights = *(int*)(data+off); off+=4;
-			lightsdata = data + off;
-			CHECK_OFF(num_lights*lightsize);
+			map_data.num_lights = *(int*)(data+off); off+=4;
+			map_data.lightsdata = data + off;
+			CHECK_OFF(map_data.num_lights*map_sizes.lightsize);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Unk9");
-			num_unk9 = *(int*)(data+off); off+=4;
-			Unknown9data = data + off;
-			CHECK_OFF(num_unk9*8);
+			map_data.num_unk9 = *(int*)(data+off); off+=4;
+			map_data.Unknown9data = data + off;
+			CHECK_OFF(map_data.num_unk9*8);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Spawn");
-			num_spawn = *(int*)(data+off); off+=4;
-			spawndata = data + off;
-			CHECK_OFF(num_spawn*spawnsize);
+			map_data.num_spawn = *(int*)(data+off); off+=4;
+			map_data.spawndata = data + off;
+			CHECK_OFF(map_data.num_spawn*map_sizes.spawnsize);
 		}
 		{
 			angel::Log.Print(angel::aeLog::LOG_DEBUG,"Outline");
-			num_outline = *(int*)(data+off); off+=4;
-			mapoutlinedata= data + off;
-			CHECK_OFF(num_outline*0xc);
+			map_data.num_outline = *(int*)(data+off); off+=4;
+			map_data.mapoutlinedata= data + off;
+			CHECK_OFF(map_data.num_outline*0xc);
 		}
 		if( off != datasize )return false;
 		return true;
@@ -108,29 +108,29 @@ namespace angel{
 	{
 		angel::Log.Print(angel::aeLog::LOG_DEBUG,"Try BLV6");
 		version		= 	 6;
-		facesize	= 0x50;  
-		sectorsize	= 0x74;
-		spritesize	= 0x1c;
-		lightsize	= 0x0C; 
-		spawnsize	= 0x14;
+		map_sizes.facesize	= 0x50;  
+		map_sizes.sectorsize	= 0x74;
+		map_sizes.spritesize	= 0x1c;
+		map_sizes.lightsize	= 0x0C; 
+		map_sizes.spawnsize	= 0x14;
 		if( DetectBLVVersion() ) return true;
 
 		angel::Log.Print(angel::aeLog::LOG_DEBUG,"Try BLV7");
 		version		= 	 7;
-		facesize	= 0x60;  
-		sectorsize	= 0x74;
-		spritesize	= 0x20;
-		lightsize	= 0x10; 
-		spawnsize	= 0x18;
+		map_sizes.facesize	= 0x60;  
+		map_sizes.sectorsize	= 0x74;
+		map_sizes.spritesize	= 0x20;
+		map_sizes.lightsize	= 0x10; 
+		map_sizes.spawnsize	= 0x18;
 		if( DetectBLVVersion() ) return true;
 
 		angel::Log.Print(angel::aeLog::LOG_DEBUG,"Try BLV8");
 		version		= 	 8;
-		facesize	= 0x60;  
-		sectorsize	= 0x78;
-		spritesize	= 0x20;
-		lightsize	= 0x14; 
-		spawnsize	= 0x18;
+		map_sizes.facesize	= 0x60;  
+		map_sizes.sectorsize	= 0x78;
+		map_sizes.spritesize	= 0x20;
+		map_sizes.lightsize	= 0x14; 
+		map_sizes.spawnsize	= 0x18;
 		if( DetectBLVVersion() ) return true;
 		return false;
 	}
@@ -187,206 +187,6 @@ namespace angel{
 		//OverlayManager::getSingleton().getByName("MMView/MapOverlay")->hide();
 		OverlayManager::getSingleton().getByName("MMView/blvMapFaceInfoOverlay")->hide();*/
 	}
-/*	void blvMap::DeselectAll()
-	{
-		DeselectFace();
-		DeselectSpawn();
-		DeselectEntity();
-	}
-	void blvMap::SelectFace()
-	{
-		//SetblvMapFaceInfoOverlay();
-		if( selected_face_index== -1)
-		{
-			DeselectFace();
-			return;
-		}
-		face_t &face = faces[selected_face_index];
-
-		//angel::Log << "select face " << selected_face_index << " "<< face.texture_name <<angel::aeLog::endl;
-
-		SceneNode *Node;
-		if( mSceneMgr->hasSceneNode("SelFaceNode"))
-			Node = mSceneMgr->getSceneNode("SelFaceNode");
-		else
-			Node =  mSceneMgr->getRootSceneNode()->createChildSceneNode("SelFaceNode");
-
-
-		ManualObject* manOb = mSceneMgr->createManualObject("SelFace");
-
-		manOb->setDynamic(true);
-		if(isPortal(face))
-		{
-			manOb->begin("BLV/SelectedPortal", RenderOperation::OT_TRIANGLE_LIST);
-			manOb->setVisibilityFlags(PORTALS_MASK);
-		}
-		else
-		{
-			manOb->setVisibilityFlags(WALLS_MASK);
-			manOb->begin("BLV/SelectedFace", RenderOperation::OT_TRIANGLE_LIST);
-		}
-
-		for( int j = 1 ; j < face.blvhdr.numvertex -1; j++ )
-		{
-			manOb->position(vertexes[face.vertex_idxs[0]]);
-			manOb->position(vertexes[face.vertex_idxs[j]]);
-			manOb->position(vertexes[face.vertex_idxs[j+1]]);
-		}
-		manOb->end();
-		Node->attachObject(manOb);
-		face_blv_t			facehdr = face.blvhdr;
-		faceparams_blv_t faceparams = face.blvfaceparam;
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/blvMapFaceInfoOverlay");
-		mMyOverlay->show();
-		std::stringstream s;
-		s << "Face#: "<<selected_face_index <<"\t";
-		s<< "Texture: " <<face.texture_name;
-		s << "\tAttr:"<<std::hex << std::setw(8)<<std::setfill('0')<< facehdr.attrib <<"\n";
-		s << std::dec ;
-		s<< "BBox max:\t" <<face.bbox.getMinimum() << "\nBBox min:\t" << face.bbox.getMaximum() <<"\n";
-		s<< "Plane:\t"<<face.plane.normal << "\nPlane dist:\t"<< face.plane.d <<"\n";
-		//s<< "PlaneI:\t"<<facehdr.plane<<"\n";
-		s<< "Plane7:\t"<<face.plane7<<"\n";
-		s << "Z_CALC: ";
-		s << facehdr.z_calc[0] <<"\t"<< facehdr.z_calc[1] <<"\t"<< facehdr.z_calc[2] <<"\t";
-		s << facehdr.z_calc[3] <<"\t"<< facehdr.z_calc[4] <<"\t"<< facehdr.z_calc[5] <<"\n";
-
-
-		s <<"FacetType: " <<std::hex << std::setw(2)<<std::setfill('0')<< (uint32_t)facehdr.facet_type<<"\t";
-		s << std::dec ;
-		s << "BitmapIdx: "<<facehdr.bitmap_index <<"\n";
-
-		s << "Room: "<<facehdr.room_number <<"\tRoom behind: " <<facehdr.room_behind_number;
-		s <<"\tPadding: "<< (int)facehdr.padding[0] <<"\t" << (int)facehdr.padding[1] <<"\n";
-		s << "Fparm idx: "<<facehdr.fparm_index;
-		s << "\tFacetIdx: "<<faceparams.facet_index;
-		s << "\tBitmapIdx: "<< faceparams.bitmap_index <<"\n";
-
-		s << "Fade X: "<< faceparams.fade.x << "\tY: "<< faceparams.fade.y << "\tZ: "<< faceparams.fade.z <<"\n";
-		s << "Fade base X: "<< faceparams.fade_base_x << "\tY: " << faceparams.fade_base_y << "\n";
-		s << "TextureFrameTbl idx: "<< faceparams.texture_frame_table_index;
-		s << "\tCOG: " <<faceparams.texture_frame_table_COG <<"\n";
-		s <<"#COG: "<< faceparams.cog_number;
-		s <<"\t#EVT: "<< faceparams.cog_triggered;
-		s << "\tType: "<< std::hex << std::setw(4)<<std::setfill('0')<< faceparams.cog_trigger_type <<"\n";
-		s << std::dec ;
-		s << "Light level: "<< faceparams.light_level <<"\n";
-		OverlayManager::getSingleton().getOverlayElement("MMView/MMblvMap/FaceParamData")->setCaption(s.str());
-
-	}
-	void blvMap::DeselectFace()
-	{
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/blvMapFaceInfoOverlay");
-		mMyOverlay->hide();
-		mSceneMgr->destroyManualObject("SelFace");
-		selected_face_index=-1;
-	}
-	void    blvMap::SelectEntity()
-	{
-		if( selected_ent_idx == -1 )
-			return;
-		Entity* ent_o = mSceneMgr->getEntity("Entity" + StringConverter::toString(selected_ent_idx));
-		if( !ent_o )
-		{
-			angel::Log << "could not find entity " << selected_ent_idx <<angel::aeLog::endl;
-		}else
-		{
-			ent_o->getParentSceneNode()->showBoundingBox(true);
-		}
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/ODMMapEntityInfoOverlay");
-		mMyOverlay->show();
-		std::stringstream s;
-		ent_t& ent=entites[selected_ent_idx];
-		s << "Entity#: "<< selected_ent_idx << "\t";
-		s << "Name: "<< ent.name << "\n";
-		s << "Origin: "<< ent.mm6ent.origin << "\n";
-
-		s << "DecListId#: "<< ent.mm6ent.declist_id<< "\t";
-		s << "AIattrMarkers: "<< ent.mm6ent.AIattrMarkers<< "\n";
-		s << "Facing: "<< std::setw(4)<<std::setfill('0')  << ent.mm6ent.facing<< "\n";
-		s << std::dec;
-
-		s << "Evt1: "<< ent.mm6ent.evt1<< "\t";
-		s << "Evt2: "<< ent.mm6ent.evt2<< "\n";
-		s << "Var1: "<< ent.mm6ent.var1<< "\t";
-		s << "Var2: "<< ent.mm6ent.var2<< "\n";
-		if( version > 6)
-		{
-			s << "Spec Trig: "<< std::hex << std::setw(4)<<std::setfill('0')  << ent.spec_trig<< "\t";
-			s << "padding: "<< std::hex << std::setw(4)<<std::setfill('0')  << ent.padding<< "\n";
-		}
-
-		OverlayManager::getSingleton().getOverlayElement("MMView/ODMMap/EntityParams")->
-			setCaption(s.str());
-
-	}
-	void    blvMap::DeselectEntity()
-	{
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/ODMMapEntityInfoOverlay");
-		mMyOverlay->hide();
-		if( selected_ent_idx == -1 )
-			return;
-		Entity* ent = mSceneMgr->getEntity("Entity" + StringConverter::toString(selected_ent_idx));
-		if( !ent )
-		{
-			angel::Log << "could not find entity " << selected_ent_idx <<angel::aeLog::endl;
-		}else
-		{
-			ent->getParentSceneNode()->showBoundingBox(false);
-		}
-
-		selected_ent_idx=-1;
-	}
-	void    blvMap::SelectSpawn()
-	{
-		if( selected_spawn_idx == -1 )
-			return;
-		Entity* ent_o = mSceneMgr->getEntity("Spawn" + StringConverter::toString(selected_spawn_idx));
-		if( !ent_o )
-		{
-			angel::Log << "could not find spawn " << selected_spawn_idx <<angel::aeLog::endl;
-		}else
-		{
-			ent_o->getParentSceneNode()->showBoundingBox(true);
-		}
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/ODMMapSpawnInfoOverlay");
-		mMyOverlay->show();
-		std::stringstream s;
-		spawn_t& swp=spawns[selected_spawn_idx];
-		s << "Spawn#: "<< selected_spawn_idx << "\t";
-		s << "index: "<< swp.mm6spawn.index << "\n";
-
-		s << "Origin: "<< swp.mm6spawn.origin<< "\t";
-		s << "Radius: "<< swp.mm6spawn.radius<< "\n";
-
-		s << "Type_ID: "<< std::hex << std::setw(4)<<std::setfill('0')  << swp.mm6spawn.type_id<< "\t";
-		s << "Attrib: "<< std::hex << std::setw(4)<<std::setfill('0')  << swp.mm6spawn.attrib<< "\n";
-		if( version > 6)
-		{
-			s << "Group: "<< std::hex << std::setw(8)<<std::setfill('0')  << swp.group<< "\n";
-
-		}
-		OverlayManager::getSingleton().getOverlayElement("MMView/ODMMap/SpawnParams")->
-			setCaption(s.str());
-	}
-	void    blvMap::DeselectSpawn()
-	{
-		Overlay*mMyOverlay = OverlayManager::getSingleton().getByName("MMView/ODMMapSpawnInfoOverlay");
-		mMyOverlay->hide();
-		if( selected_spawn_idx == -1 )
-			return;
-		Entity* ent = mSceneMgr->getEntity("Spawn" + StringConverter::toString(selected_spawn_idx));
-		if( !ent )
-		{
-			angel::Log << "could not find spawn " << selected_spawn_idx <<angel::aeLog::endl;
-		}else
-		{
-			ent->getParentSceneNode()->showBoundingBox(false);
-		}
-
-		selected_spawn_idx=-1;
-
-	}*/
 
 	void blvMap::Select()
 	{
